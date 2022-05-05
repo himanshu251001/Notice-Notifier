f471ce240e3aae4a550e182d7018bfb265a01eaf
himanshu251001 <79706463+himanshu251001@users.noreply.github.com>
2022-05-06 00:19:20 +0530

    Add files via upload

--- extract.js
+++ extract.js
@@ -1,18 +1,21 @@
-fetch('testNotice.json')
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    appendData(data);
-})
-.catch(function (err) {
-    console.log('error: ' + err);
-});
-function appendData(data) {
-var mainContainer = document.getElementById("myData");
-for (var i = 0; i < data.length; i++) {
-    var div = document.createElement("div");
-    div.innerHTML = data[i];
-    mainContainer.appendChild(div);
+async function extract() {
+
+    var res = await fetch('testNotice.json');
+    var data = await res.json()
+    var mainContainer = document.getElementById("myData");
+
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("a");
+        div.innerHTML = data[i];
+        mainContainer.appendChild(div);
+    }
+    res = await fetch('testLink.json');
+    data = await res.json()
+    
+    var childs = mainContainer.children;
+    for (var i = 0; i < data.length; i++) {
+            childs[i].setAttribute('href', data[i]);
+            childs[i].setAttribute('target', "_blank");
+    }
 }
-}
+extract();
